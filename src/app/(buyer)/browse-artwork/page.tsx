f63716e5b1681284f0b6aa188587fa7d86ed1bb3
abhishake1 <abhi9848038283@gmail.com>
2024-03-08 13:19:52 +0000

    style: search

--- src/app/(buyer)/browse-artwork/page.tsx
+++ src/app/(buyer)/browse-artwork/page.tsx
@@ -6,7 +6,7 @@ export default async function Page({ searchParams }: { searchParams: { q?: strin
   const artworks = await api.artwork.search.query(searchParams.q)
 
   return <div className="relative w-full pt-36">
-    <div className="absolute top-20 right-10 w-96 flex flex-row space-x-3 justify-end items-center">
+    <div className="sticky z-10 ml-auto pr-4 top-20 w-96 flex flex-row space-x-3 justify-end items-center">
       <div className="relative">
         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
           <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -16,7 +16,7 @@ export default async function Page({ searchParams }: { searchParams: { q?: strin
         <ArtworkSearchBar search={searchParams.q} />
       </div>
     </div>
-    <div className="grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-4 gap-4 max-w-7xl mx-auto">{
+    <div className="grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-4 gap-4 max-w-7xl mx-auto justify-items-center">{
       artworks?.map(({ name, id, image, style, size, price }) => {
         return <DirectionAwareHover key={id} imageUrl={image?.url ?? ''}>
           <p className="font-bold text-xl">{name}</p>
