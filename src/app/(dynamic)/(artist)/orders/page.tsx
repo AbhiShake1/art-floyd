cc3a5782f7afd5bfbf8a482afaa29ffdf098de84
abhishake1 <abhi9848038283@gmail.com>
2024-03-10 17:49:09 +0000

    fix: build error

--- src/app/(dynamic)/(artist)/orders/page.tsx
+++ src/app/(dynamic)/(artist)/orders/page.tsx
@@ -2,6 +2,8 @@ import { api } from "~/trpc/server";
 import { OrdersTable } from "./_components/table";
 import SuperJSON from "superjson";
 
+export const dynamic = "force-dynamic"
+
 export default async function Page() {
   const orders = await api.order.my.query()
 
