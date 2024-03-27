60710d8ed4aa2b542eed360035add3664067ed49
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2024-03-27 10:58:04 +0545

    style(browse artwork): change `settings` to `categories`

--- src/app/(dynamic)/(buyer)/browse-artwork/page.tsx
+++ src/app/(dynamic)/(buyer)/browse-artwork/page.tsx
@@ -33,7 +33,7 @@ export default async function Page({ searchParams: { q, sort, style } = {} }: { 
     </div>
     <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col items-start justify-start gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
       <div className="mx-auto grid w-full">
-        <h1 className="text-3xl font-semibold">Settings</h1>
+        <h1 className="text-3xl font-semibold">Categories</h1>
       </div>
       <div className="mx-auto grid w-full items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
         <nav className="grid gap-4 text-sm text-muted-foreground">
